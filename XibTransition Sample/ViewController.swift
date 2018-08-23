362522436f3dd6c732d2d9a685d8afe7a21e0d4a
西村　拓人 <takuto_nishimura@waku-2.com>
2018-08-23 11:43:44 +0900

    Initial Commit

---
+++ XibTransition Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XibTransition Sample
+//
+//  Created by 西村　拓人 on 2018/08/23.
+//  Copyright © 2018年 西村　拓人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
